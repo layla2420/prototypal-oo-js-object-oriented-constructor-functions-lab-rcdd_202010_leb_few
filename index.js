664eaf7e85eab63557a3f7e30a39e580c8b448bb
layla  hammoud <laylahammoud63@gmail.com>
2020-12-18 18:10:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function Scooterconstructor(year,color,modol){
+function Scooter(year,color,modol){
   return{
   year,
   color,
   modol,
 }
+}
+function Driver(name,age,experience) {
+return{
+  name,
+  age,
+  experience
+}  
+}
+function PickupLocation(adress,city){
+  return{
+    adress,
+    city
+  }
 }
